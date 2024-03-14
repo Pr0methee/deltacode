059ba78f6a806503d6e64cee38eb9d28c9d9d0d1
Pr0methee <127605932+Pr0methee@users.noreply.github.com>
2024-03-14 14:59:14 +0100

    Add files via upload

--- test_number.py
+++ test_number.py
@@ -1,3 +1,4 @@
+import copy
 class Number:
     def __init__(self,sgn:bool,ent:list[int],frac:list[int]):
         """
@@ -134,6 +135,114 @@ class Number:
         r = __other-self
         r.sgn = not r.sgn
         return r
+
+    def __mul__(self,__o):
+        assert type(__o) == Number
+        
+        if __o == Number(False,[0],[0]) or self == Number(False,[0],[0]):
+            return Number(False,[0],[0])
+        
+        if self == Number(False,[1],[0]):
+            return copy.copy(__o)
+        
+        if __o == Number(False,[1],[0]):
+            return copy.copy(self)
+        
+        if self.frac == [0 for _ in range(20)] and self.ent[-1] ==0:
+            a=Number(self.sgn,self.ent[:-1],[0])
+            entb=__o.ent+[__o.frac[0]]
+            
+            fracb=__o.frac[1:]
+            b = Number(__o.sgn,entb,fracb)
+            return a*b
+        if __o.frac == [0 for _ in range(20)] and __o.ent[-1] ==0:
+            return __o*self
+
+
+        
+        la = self.ent+self.frac
+        lb = __o.ent + __o.frac
+        while len(la)>len(lb):
+            lb = [0]+lb
+        while len(lb)>len(la):
+            la = [0]+la
+        lc=[]
+        for k in range(len(la)):
+            i = len(la)-1-k
+            if la[i]==0:continue
+            j = len(lb) - 1
+            act=[0 for _ in range(k)]
+            while j >=0:
+                act = [lb[j]*la[i]]+act
+                j-=1
+
+            for j in range(len(act)-1,-1,-1):
+                i =0
+                while act[j]>=10:
+                    i+=1
+                    act[j]-=10
+                if j == 0 :
+                    act = [i]+act
+                else:
+                    act[j-1] += i
+
+            while len(act)< 41:
+                act=[0]+act
+            lc.append(Number40(act[:-40],act[-40:]))
+        
+        S=Number40([0],[0])
+        for elt in lc:
+            S += elt
+        r=S.troncate()
+        r.sgn=(self.sgn!=__o.sgn)
+        return r
+
+    def __truediv__(self,__o):
+        assert type(__o)==Number
+        if __o == Number(True,[1],[0]):
+            return Number(not self.sgn,self.ent,self.frac)
+        if __o == Number(False,[1],[0]):
+            return copy.copy(self)
+        if self == Number(False,[0],[0]):
+            return Number(False,[0],[0])
+        if __o == Number(False,[0],[0]):
+            raise ZeroDivisionError
+        if __o.frac != [0 for  _ in range(20)]:
+            a = Number(False,[1,0],[0]) * self
+            b = Number(False,[1,0],[0]) * __o
+            return a/b
+        if __o.ent[-1] ==0:
+            a = Number(self.sgn, [0]+self.ent[:-1],[self.ent[-1]]+ self.frac[:-1])
+            b = Number(__o.sgn, [0]+__o.ent[:-1],[self.__o[-1]]+ __o.frac[:-1])
+            return a/b
+        
+        la = self.ent+self.frac
+        entc,fracc= [],[]
+        i = len(__o.ent)
+        act =la [:i]
+        n = Number(False,act,[0])
+        f = False #flag, est-ce qu'on a commencé a remplir la partie decimale
+        while len(fracc) != 20:
+            k=Number(False,[0],[0])
+            while k*__o-n<Number(False,[0],[0]):
+                k+=Number(False,[1],[0])
+            k -= Number(False,[1],[0])
+            m = __o * k
+            n -= m
+            n *= Number(False,[1,0],[0])
+            if i ==len(la):
+                la.append(0)
+            n += Number(False,[la[i]],[0])
+            i += 1
+            if f :
+                fracc.append(k.ent[0])
+            else:
+                entc.append(k.ent[0])
+            
+            if i > len(self.ent) and not f:
+                f = True
+        return Number(self.sgn != __o.sgn, entc,fracc)
+
                 
     def __repr__(self):
         return self.__str__()
@@ -154,20 +263,73 @@ class Number:
     def recognize(ch:str):
         return all([car in '0123456789,-' for car in ch])
 
+
+
+class Number40:
+    def __init__(self,ent:list[int],frac:list[int]):
+        """
+        Toujours positif !
+        Nombre avec 40 chiffres après la virgule
+        Sert seulement pour multiplier !
+        """
+        assert type(ent)==type(frac)==list and len(frac)<=40
+        assert all([type(elt)==int for elt in ent]) and all([type(elt)==int for elt in frac])
+        assert all([0<=elt<10 for elt in ent]) and all([0<=elt<10 for elt in frac])
+        self.ent=ent#partie entière
+        while self.ent[0]==0 and len(self.ent)>1:
+            self.ent.pop(0)
+        self.frac=frac #partie decimale
+        while len(self.frac) != 40:
+            self.frac.append(0)
+
+    
+    def __add__(self,__other):
+        assert type(__other)==Number40
+
+        if __other == Number40([0],[0]):
+            return Number(self.sgn,self.ent,self.frac)
+        
+
+        enta=self.ent
+        entb=__other.ent
+        while len(enta) <len(entb):
+            enta=[0]+enta
+        while len(entb) <len(enta):
+            entb=[0]+entb
+        ent=[enta[i]+entb[i] for i in range(len(enta))]
+        frac=[self.frac[i]+__other.frac[i] for i in range(40)]
+        for k in range(39,0,-1):
+            if frac[k]>=10:
+                frac[k]-=10
+                frac[k-1]+=1
+        if frac[0]>=10:
+            frac[0]-=10
+            ent[-1]+=1
+        for k in range(len(ent)-1,0,-1):
+            if ent[k]>=10:
+                ent[k]-=10
+                ent[k-1]+=1
+        if ent[0]>=10:
+            ent[0]-=10
+            ent=[1]+ent
+        return Number40(ent,frac)
+
+    def troncate(self):
+        return Number(False,self.ent,self.frac[:20])
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     a =Number(False,[1],[0])
-    a_ = Number.from_str("1")
-    assert a==a_
     b = Number(False,[2],[2,3])
-    b_ = Number.from_str("2,23")
-    assert b==b_
     c = Number(True,[2],[2,0,4])
-    c_ = Number.from_str('-2,204')
-    assert c==c_
-    print('a=',a,';b=',b,';c=',c)
-    print('a+a=',a+a)
-    print('a-a=',a-a)
-    print('a+b=',a+b)
-    print('a-b=',a-b)
-    print('a+c=',a+c)
-    print('a-c=',a-c)
+    d= Number.from_str("10")
+    print(d*b)
+    print("a/b=",a/b)
